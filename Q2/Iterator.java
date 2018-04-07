dcd651d8d684140d79ecfe520fa4d72aa8a664c6
Theo VdB <theo.a.vdb@gmail.com>
2018-04-07 19:38:02 -0400

    implemented 2.1 and 2,2 requirments from intructions PDF

--- Q2/Iterator.java
+++ Q2/Iterator.java
@@ -1,4 +1,6 @@
 public interface Iterator<E> {
     E next();
     boolean hasNext();
+    int nextIndex();
+    Iterator <E> itorator(int nextIndex);
 }
--- Q2/LinkedList.java
+++ Q2/LinkedList.java
@@ -2,83 +2,114 @@ import java.util.NoSuchElementException;
 
 public class LinkedList <E> implements List <E> {
 
-    private static class Node <T> {
+  private static class Node <T> {
 
-        private T value;
-        private Node <T> prev;
-        private Node <T> next;
+    private T value;
+    private Node <T> prev;
+    private Node <T> next;
 
-        private Node (T value, Node <T> prev, Node <T> next) {
-            this.value = value;
-            this.prev = prev;
-            this.next = next;
-        }
+    private Node (T value, Node <T> prev, Node <T> next) {
+      this.value = value;
+      this.prev = prev;
+      this.next = next;
+    }
+  }
+
+  private Node <E> head;
+  private int size;
+
+  public LinkedList () {
+    head = new Node <E> (null, null, null); // dummy node!
+    head.prev = head.next = head;
+    size = 0;
+  }
+
+  private class LinkedListIterator implements Iterator <E> {
+
+    private Node <E> current = head;
+    private int pointer;
+
+    public boolean hasNext () {
+      return (current.next != head);
     }
 
-    private Node <E> head;
-    private int size;
+    public E next () {
 
-    public LinkedList () {
-        head = new Node <E> (null, null, null); // dummy node!
-        head.prev = head.next = head;
-        size = 0;
+      if (! hasNext ()) {
+        throw new NoSuchElementException ();
+      }
+
+      current = current.next;
+
+      return current.value;
     }
 
-    private class LinkedListIterator implements Iterator <E> {
-
-        private Node <E> current = head;
-
-        public boolean hasNext () {
-            return (current.next! = head);
-        }
-
-        public E next () {
-
-            if (! hasNext ()) {
-                throw new NoSuchElementException ();
-            }
-
-            current = current.next;
-
-            return current.value;
-        }
+    public int nextIndex(){
+      Node <E> current = head.next;
+      for (int i = 0; i<=pointer ; i ++) {
+        pointer++;
+      }
+      return pointer++;
     }
 
-    public Iterator <E> iterator () {
-        return new LinkedListIterator ();
+    public Iterator <E> itorator(int nexIndex){
+      try{
+        this.nextIndex();//probaby does not work
+      }catch (IndexOutOfBoundsException someName) {
+        return null;
+      }
+      LinkedListIterator temp=new LinkedListIterator();
+    //set pointer to the one beofre nextIndex
+      return null;//should not be null
     }
 
-    public int size () {
-        return size;
+  }
+
+  public Iterator <E> iterator () {
+    return new LinkedListIterator ();
+  }
+
+
+  public int size () {
+    return size;
+  }
+
+  public E get (int index) {
+
+    if (index <0 || index >= size) {
+      throw new IndexOutOfBoundsException (Integer.toString (index));
     }
 
-    public E get (int index) {
+    Node <E> p = head.next;
 
-        if (index <0 || index> = size) {
-            throw new IndexOutOfBoundsException (Integer.toString (index));
-        }
-
-        Node <E> p = head.next;
-
-        for (int i = 0; i <index; i ++) {
-            p = p.next;
-        }
-
-        return p.value;
+    for (int i = 0; i <index; i ++) {
+      p = p.next;
     }
 
-    public void addFirst (E elem) {
+    return p.value;
+  }
 
-        if (elem == null) {
-            throw new NullPointerException ();
-        }
+  public void addFirst (E elem) {
 
-        Node <E> second = head.next;
-
-        head.next = new Node <E> (elem, head, second);
-        second.prev = head.next;
-
-        size ++;
+    if (elem == null) {
+      throw new NullPointerException ();
     }
+
+    Node <E> second = head.next;
+
+    head.next = new Node <E> (elem, head, second);
+    second.prev = head.next;
+
+    size ++;
+  }
+
+  public void add(E elem){
+    Node <E> temp = head.prev;
+    temp.prev=head.prev;
+    temp.next = head;
+    head.prev = temp;
+    size ++;
+  }
+
 
 }
