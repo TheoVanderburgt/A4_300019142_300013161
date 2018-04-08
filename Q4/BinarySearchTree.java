a60d9fe0100d84e62cf709b5740ea4c36b9930f3
Theo VdB <theo.a.vdb@gmail.com>
2018-04-08 16:03:38 -0400

    done with a few small issues when running Q4

--- Q4/BinarySearchTree.java
+++ Q4/BinarySearchTree.java
@@ -6,84 +6,129 @@ import java.util.NoSuchElementException;
 
 public class BinarySearchTree<E extends Comparable<E>> {
 
-    private static class Node<T> {
-        private T value;
-        private Node<T> left;
-        private Node<T> right;
-        private Node(T value) {
-            this.value = value;
-            left = null;
-            right = null;
-        }
+  private static class Node<T> {
+    private T value;
+    private Node<T> left;
+    private Node<T> right;
+    private Node(T value) {
+      this.value = value;
+      left = null;
+      right = null;
+    }
+  }
+
+  private Node<E> root = null;
+
+  /**
+  * Inserts an object into this BinarySearchTree.
+  *
+  * @param elem item to be added
+  * @return true if the object has been added and false otherwise
+  */
+
+  public boolean add(E elem) {
+
+    // pre-condtion:
+
+    if (elem == null) {
+      throw new NullPointerException();
     }
 
-    private Node<E> root = null;
+    // special case:
 
-    /**
-    * Inserts an object into this BinarySearchTree.
-    *
-    * @param elem item to be added
-    * @return true if the object has been added and false otherwise
-    */
-
-    public boolean add(E elem) {
-
-        // pre-condtion:
-
-        if (elem == null) {
-            throw new NullPointerException();
-        }
-
-        // special case:
-
-        if (root == null) {
-            root = new Node<E>(elem);
-            return true;
-        }
-
-        // general case:
-
-        return add(elem, root);
+    if (root == null) {
+      root = new Node<E>(elem);
+      return true;
     }
 
-    // Helper method
+    // general case:
 
-    private boolean add(E elem, Node<E> current) {
+    return add(elem, root);
+  }
 
-        boolean result;
-        int test = elem.compareTo(current.value);
+  // Helper method
 
-        if (test == 0) {
-            result = false; // already exists, not added
-        } else if (test < 0) {
-            if (current.left == null) {
-                current.left = new Node<E>(elem);
-                result = true;
-            } else {
-                result = add(elem, current.left);
-            }
-        } else {
-            if (current.right == null) {
-                current.right = new Node<E>(elem);
-                result = true;
-            } else {
-                result = add(elem, current.right);
-            }
-        }
-        return result;
+  private boolean add(E elem, Node<E> current) {
+
+    boolean result;
+    int test = elem.compareTo(current.value);
+
+    if (test == 0) {
+      result = false; // already exists, not added
+    } else if (test < 0) {
+      if (current.left == null) {
+        current.left = new Node<E>(elem);
+        result = true;
+      } else {
+        result = add(elem, current.left);
+      }
+    } else {
+      if (current.right == null) {
+        current.right = new Node<E>(elem);
+        result = true;
+      } else {
+        result = add(elem, current.right);
+      }
+    }
+    return result;
+  }
+
+  //Create method add
+  public int count(E low, E high){
+    int counter=0;
+    int test;
+    Node<E> current =root;
+    boolean foundLow= false;
+    return count(counter,low,high,current,foundLow);
+//private helper case
+  }
+  private int count(int counter,E low, E high,Node<E> current, boolean foundLow){
+    if (foundLow==false){ //this finds lowest element
+      if (current.value==low){
+        counter++;
+        current=root;
+        return count(counter,low,high,current,true);//current.root does not exsist
+      }else if (low.compareTo(current.left.value)>0){
+        return count(counter,low,high,current.left,false);
+      }else{
+        return count(counter,low,high,current.right,false);
+      }
     }
 
-    public String toString() {
-        return toString(root);
+    if (low.compareTo(current.left.value)>0  &&  high.compareTo(current.left.value)<0){
+      counter++;
+      if (current.right.value==null){
+        current=root;
+      }
+      return count(counter,low,high,current.left,true);
+    }
+    if (low.compareTo(current.right.value)>0  &&  high.compareTo(current.right.value)<0){
+      counter++;
+      if (current.right.value==null){
+        current=root;
+      }
+      return count(counter,low,high,current.right,true);
+    }
+    if (current.value==high){
+      return counter++;
+    }
+    return counter;
+  }
+
+
+
+
+  public String toString() {
+    return toString(root);
+  }
+
+  private String toString(Node<E> current) {
+
+    if (current == null) {
+      return "()";
     }
 
-    private String toString(Node<E> current) {
-
-        if (current == null) {
-            return "()";
-        }
-
-        return "(" + toString(current.left) + current.value + toString(current.right) + ")";
-    }
+    return "(" + toString(current.left) + current.value + toString(current.right) + ")";
+  }
 
 }
